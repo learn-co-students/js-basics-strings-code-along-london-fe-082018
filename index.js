174ef5e186ab48f539b1d3bbe616fb9161ef3499
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-19 19:49:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+var welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + "!";
+var shortGreeting = 'Welcome, ' + 'G' + "!";
