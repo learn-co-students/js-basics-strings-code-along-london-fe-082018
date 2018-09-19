93337a4f20f8429a47eff2b338a0c799e2f03e25
active-tutorial-1080 <gareth.shackleton@wework.com>
2018-09-19 19:47:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+let currentUser = "Gareth"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1)
+let shortGreeting = "Welcome, " + firstInitial + "!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
